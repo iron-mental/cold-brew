abf9deb7005352daa9ff1b2241144fb55fee8746
cheol-95 <rkdcjf0122@gmail.com>
2020-09-15 18:23:59 +0900

    init

--- router/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-
-router.get('/', (req, res) => {
-  res.send('<h3>"/"</h3>');
-});
-
-module.exports = router;
