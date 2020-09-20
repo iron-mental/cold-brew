34d994b8be71ff413574fce1f6806e88a94a0476
cheol-95 <rkdcjf0122@gmail.com>
2020-09-21 03:24:56 +0900

    feat: auth 테스트

--- test/sign.test.js
+++ test/sign.test.js
@@ -1,41 +1,70 @@
 const should = require('should');
 const request = require('supertest');
-const app = require('./app');
+const app = require('../app');
 
-describe('GET /', () => {
-  it('문자열을 반환한다', done => {
+describe('POST /v1/signup', () => {
+  it('회원가입시 uid를 보유한 객체를 반환한다', done => {
     request(app)
-      .get('/')
+      .post('/v1/signup')
+      .send({
+        email: 'user_email@email.com',
+        password: 'user_password',
+        nickname: 'user_nickname',
+      })
       .end((err, res) => {
-        console.log(res.text);
-        // res.body.should.be.instanceof(Object);
-        res.text.should.be.instanceof(String);
+        result = res.body;
+        result.should.be.an.instanceOf(Object).and.have.property('uid');
+        result.uid.should.be.an.instanceOf(String);
       });
     done();
   });
 });
 
-// describe('GET /users', () => {
-//   describe('성공', () => {
-//     it('유저 객체 전부 반환', done => {
-//       request(app)
-//         .get('/users')
-//         .end((err, res) => {
-//           res.body.should.be.instanceof(Array);
-//           res.body.forEach(user => {
-//             user.should.have.property('name');
-//           });
-//           done();
-//         });
-//     });
-//     it('특정 유저 객체를 반환한다', done => {
-//       request(app)
-//         .get('/users/1')
-//         .end((err, res) => {
-//           res.body.should.have.property('id', 1);
-//           done();
-//         });
-//     });
+describe('POST /v1/login', () => {
+  it('로그인시 uid를 보유한 객체를 반환한다', done => {
+    request(app)
+      .post('/v1/login')
+      .send({
+        email: 'user_email@email.com',
+      })
+      .end((err, res) => {
+        result = res.body;
+        result.should.be.an.instanceOf(Object).and.have.property('uid');
+        result.uid.should.be.an.instanceOf(String);
+      });
+    done();
+  });
+});
+
+// describe('PATCH /v1/update', () => {
+//   it('회원정보 수정시 유저 레코드를 반환한다', done => {
+//     request(app)
+//       .patch('/v1/update')
+//       .send({
+//         uid: ,
+//         contents: {
+//           email: 'changed_email@gmail.com',
+//         },
+//       })
+//       .end((err, res) => {
+//         result = res.body;
+//         result.should.be.an.instanceOf(Object).and.have.property('uid');
+//       });
+//     done();
 //   });
-//   // describe('실패', () => {});
+// });
+
+// describe('DELETE /v1/signout', () => {
+//   it('로그아웃 성공시 성공했다는 문자열을 반환한다', done => {
+//     request(app)
+//       .delete('/v1/signout')
+//       .send({
+//         uid: uid,
+//       })
+//       .end((err, res) => {
+//         result = res.body;
+//         result.should.be.an.instanceOf(String);
+//       });
+//     done();
+//   });
 // });
