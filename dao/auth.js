a62e8e138207dbe53982f3253491a8607796cf60
cheol-95 <rkdcjf0122@gmail.com>
2020-09-22 20:49:46 +0900

    refector: query escape 적용

--- dao/auth.js
+++ dao/auth.js
@@ -1,24 +1,32 @@
 const pool = require('./db');
 
 const signup = async (body, next) => {
-  const { uid, email, nickname } = body;
-  const sql = `insert into user(uid, email, nickname) values ('${uid}', '${email}', '${nickname}')`;
+  const conn = await pool.getConnection();
+  const sql = `insert into user(uid, email, nickname) values (?, ?, ?)`;
+  const params = [body.uid, body.email, body.nickname];
   try {
-    const [rows] = await pool.query(sql);
+    const [rows] = await conn.query(sql, params);
+    if (!rows.affectedRows) throw { sqlMessage: '처리결과없음' };
     return rows;
   } catch (err) {
     throw next({ status: 400, message: err.sqlMessage });
+  } finally {
+    await conn.release();
   }
 };
 
 const withdraw = async (body, next) => {
-  const { uid } = body;
-  const sql = `delete from user where uid = '${uid}'`;
+  const conn = await pool.getConnection();
+  const sql = `delete from user where uid = ?`;
+  const params = [body.uid];
   try {
-    const [rows] = await pool.query(sql);
+    const [rows] = await conn.query(sql, params);
+    if (!rows.affectedRows) throw { sqlMessage: '처리결과없음' };
     return rows;
   } catch (err) {
     throw next({ status: 400, message: err.sqlMessage });
+  } finally {
+    await conn.release();
   }
 };
 
