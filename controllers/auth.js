e80fbe454f571b81f5bbd18aa226ca3423e22e65
cheol-95 <rkdcjf0122@gmail.com>
2020-09-22 20:43:02 +0900

    refector: 리턴 형식 변경

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 var authService = require('../services/auth');
 
 const signup = async (req, res, next) => {
-  rows = await authService.signup(req.body, next);
-  return res.status(201).json({ message: '리디렉션으로 변경예정' });
+  await authService.signup(req.body, next);
+  return res.status(201).json({ message: 'success' });
 };
 
 const withdraw = async (req, res, next) => {
-  rows = await authService.withdraw(req.body, next);
+  await authService.withdraw(req.body, next);
   return res.status(200).json({ message: 'success' });
 };
 
