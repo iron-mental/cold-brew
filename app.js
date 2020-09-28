1f6b6a02e6ea4df04712fac1498c62c187bcf5ca
cheol-95 <rkdcjf0122@gmail.com>
2020-09-28 20:36:58 +0900

    fix: 에러로그 추가

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if (!err.status) {
+    console.error(err);
+  }
+
   res.status(err.status || 500);
   return res.json({ message: err.message });
 });
