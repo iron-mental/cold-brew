c791fecd3e9a396ceae57d157d6357411e8aeaf3
cheol-95 <rkdcjf0122@gmail.com>
2020-10-05 15:44:25 +0900

    fix: 파일 확장자를 소문자 변환

--- controllers/study.js
+++ controllers/study.js
@@ -2,7 +2,10 @@ const studyService = require('../services/study');
 const path = require('path');
 
 const createStudy = async (req, res) => {
-  req.body.image = path.normalize(path.join('/images/study', req.file.originalname));
+  const basename = path.basename(req.file.originalname).split('.').slice(0, -1).join('.');
+  const extension = path.extname(req.file.originalname).toLowerCase();
+  req.body.image = path.normalize(path.join('/images/study', basename)).concat(extension);
+
   await studyService.createStudy(req.body);
   return res.status(201).json({ message: '스터디 생성 성공' });
 };
