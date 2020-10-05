29abbd127d604280baace576a0a4a742f54466e4
cheol-95 <rkdcjf0122@gmail.com>
2020-10-05 15:58:03 +0900

    fix: 소문자 변환 제거

--- controllers/study.js
+++ controllers/study.js
@@ -2,9 +2,7 @@ const studyService = require('../services/study');
 const path = require('path');
 
 const createStudy = async (req, res) => {
-  const basename = path.basename(req.file.originalname).split('.').slice(0, -1).join('.');
-  const extension = path.extname(req.file.originalname).toLowerCase();
-  req.body.image = path.normalize(path.join('/images/study', basename)).concat(extension);
+  req.body.image = path.normalize(path.join('/images/study', req.file.originalname));
 
   await studyService.createStudy(req.body);
   return res.status(201).json({ message: '스터디 생성 성공' });
