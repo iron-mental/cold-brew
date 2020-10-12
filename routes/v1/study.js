7f2e02339d14bf85b8a899e0ebb5ac727319df76
cheol-95 <rkdcjf0122@gmail.com>
2020-10-12 21:08:47 +0900

    fix: 라우팅 수정

--- routes/v1/study.js
+++ routes/v1/study.js
@@ -5,9 +5,9 @@ const asyncWrap = require('../../utils/errors/wrap');
 const noticeValid = require('../../utils/validators/notice');
 const noticeController = require('../../controllers/notice');
 
-router.post('/:studyId/notice', noticeValid.createNotice, asyncWrap(noticeController.createNotice)); // 공지 생성 (권한확인 추가할것)
-router.get('/:studyId/notice/:noticeId', noticeValid.noticeDetail, asyncWrap(noticeController.noticeDetail)); // 공지 조회
-router.patch('/:studyId/notice/:noticeId', noticeValid.noticeUpdate, asyncWrap(noticeController.noticeUpdate)); // 공지 수정 (권한확인 추가할것)
-router.delete('/:studyId/notice/:noticeId', noticeValid.noticeDelete, asyncWrap(noticeController.noticeDelete)); // 공지 삭제  (권한확인 추가할것)
+router.post('/:study_id/notice', noticeValid.createNotice, asyncWrap(noticeController.createNotice)); // 공지 생성 (권한확인 추가할것)
+router.get('/:study_id/notice/:notice_id', noticeValid.noticeDetail, asyncWrap(noticeController.noticeDetail)); // 공지 조회
+router.patch('/:study_id/notice/:notice_id', noticeValid.noticeUpdate, asyncWrap(noticeController.noticeUpdate)); // 공지 수정 (권한확인 추가할것)
+router.delete('/:study_id/notice/:notice_id', noticeValid.noticeDelete, asyncWrap(noticeController.noticeDelete)); // 공지 삭제  (권한확인 추가할것)
 
 module.exports = router;
--- services/notice.js
+++ services/notice.js
@@ -15,7 +15,6 @@ const noticeDetail = async ({ study_id, notice_id }) => {
   if (noticeData.length === 0) {
     throw { status: 400, message: '조회 결과가 없습니다' };
   }
-
   noticeData[0].created_at = format(noticeData[0].created_at, 'yyyy-MM-dd HH:mm:ss');
   noticeData[0].updated_at = format(noticeData[0].updated_at, 'yyyy-MM-dd HH:mm:ss');
   return noticeData;
@@ -24,13 +23,13 @@ const noticeDetail = async ({ study_id, notice_id }) => {
 const noticeUpdate = async ({ study_id, notice_id }, updateData) => {
   const rows = await noticeDao.noticeUpdate(study_id, notice_id, updateData);
   if (rows.affectedRows === 0) {
-    throw { status: 400, message: '공지사항 작성에 실패했습니다' };
+    throw { status: 400, message: '공지사항 수정에 실패했습니다' };
   }
 };
 
 const noticeDelete = async ({ study_id, notice_id }) => {
   const rows = await noticeDao.noticeDelete(study_id, notice_id);
-  if (rows.length === 0) {
+  if (rows.affectedRows === 0) {
     throw { status: 400, message: '삭제에 실패했습니다' };
   }
 };
