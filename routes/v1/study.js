950ce7e0939902e71d700d9b1c56c48ff9e3d1b8
cheol-95 <rkdcjf0122@gmail.com>
2020-10-12 21:23:02 +0900

    fix: 라우팅

--- routes/v1/study.js
+++ routes/v1/study.js
@@ -5,9 +5,9 @@ const asyncWrap = require('../../utils/errors/wrap');
 const applyValid = require('../../utils/validators/apply');
 const applyController = require('../../controllers/apply');
 
-router.post('/:studyId/apply', applyValid.createApply, asyncWrap(applyController.createApply)); // 스터디 신청
-router.get('/:studyId/apply/:applyId', applyValid.applyDetail, asyncWrap(applyController.applyDetail)); // 신청 조회
-router.patch('/:studyId/apply/:applyId', applyValid.applyUpdate, asyncWrap(applyController.applyUpdate)); // 신청 수정 (reject 권한확인 추가할것)
-router.delete('/:studyId/apply/:applyId', applyValid.applyDelete, asyncWrap(applyController.applyDelete)); // 신청 삭제  (정책에 따라 없어질수도 있음)
+router.post('/:study_id/apply', applyValid.createApply, asyncWrap(applyController.createApply)); // 스터디 신청
+router.get('/:study_id/apply/:apply_id', applyValid.applyDetail, asyncWrap(applyController.applyDetail)); // 신청 조회
+router.patch('/:study_id/apply/:apply_id', applyValid.applyUpdate, asyncWrap(applyController.applyUpdate)); // 신청 수정 (reject 권한확인 추가할것)
+router.delete('/:study_id/apply/:apply_id', applyValid.applyDelete, asyncWrap(applyController.applyDelete)); // 신청 삭제  (정책에 따라 없어질수도 있음)
 
 module.exports = router;
