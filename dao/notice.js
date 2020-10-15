cb4f464b346d833b9ba564e11f3e6dbbd89a9266
cheol-95 <rkdcjf0122@gmail.com>
2020-10-15 16:21:40 +0900

    fix: 풀 커넥션 반환처리

--- dao/notice.js
+++ dao/notice.js
@@ -2,53 +2,57 @@ const pool = require('./db');
 
 const createNotice = async (createData) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const createSQL = 'INSERT INTO notice SET ?';
     const [createRows] = await conn.query(createSQL, createData);
-    await conn.release();
     return createRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const noticeDetail = async (study_id, notice_id) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const detailSQL = `SELECT id, study_id, title, contents, pined,
       FROM_UNIXTIME(UNIX_TIMESTAMP(created_at),'%Y-%m-%d %H:%i:%s') AS created_at,
       FROM_UNIXTIME(UNIX_TIMESTAMP(updated_at),'%Y-%m-%d %H:%i:%s') AS updated_at 
       FROM notice 
       WHERE study_id = ? AND id = ?`;
     const [detailRows] = await conn.query(detailSQL, [study_id, notice_id]);
-    await conn.release();
     return detailRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const noticeUpdate = async (study_id, notice_id, updateData) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const updateSQL = 'UPDATE notice SET ? WHERE study_id = ? AND id = ?';
     const [updateRows] = await conn.query(updateSQL, [updateData, study_id, notice_id]);
-    await conn.release();
     return updateRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const noticeDelete = async (study_id, notice_id) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const deleteSQL = 'DELETE FROM notice WHERE study_id = ? AND id = ?';
     const [deleteRows] = await conn.query(deleteSQL, [study_id, notice_id]);
-    await conn.release();
     return deleteRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
