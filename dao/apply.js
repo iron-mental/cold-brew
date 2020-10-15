5762f3ec5566a91c675b870a4a18fc72e2e11c9f
cheol-95 <rkdcjf0122@gmail.com>
2020-10-15 16:59:31 +0900

    fix: 풀 커넥션 반환처리

--- dao/apply.js
+++ dao/apply.js
@@ -2,53 +2,58 @@ const pool = require('./db');
 
 const createApply = async (createData) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const createSQL = 'INSERT INTO apply SET ?';
     const [createRows] = await conn.query(createSQL, createData);
-    await conn.release();
     return createRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const applyDetail = async (study_id, apply_id) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const detailSQL = `
     SELECT id, user_id, study_id, message, rejected_status,
     FROM_UNIXTIME(UNIX_TIMESTAMP(created_at),'%Y-%m-%d %H:%i:%s') AS created_at,
     FROM_UNIXTIME(UNIX_TIMESTAMP(rejected_at),'%Y-%m-%d %H:%i:%s') AS rejected_at
-    FROM apply WHERE study_id = ? AND id = ?`;
+    FROM apply
+    WHERE study_id = ? AND id = ?`;
     const [detailRows] = await conn.query(detailSQL, [study_id, apply_id]);
-    await conn.release();
     return detailRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const applyUpdate = async (study_id, apply_id, updateData) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const updateSQL = 'UPDATE apply SET ? WHERE study_id = ? AND id = ?';
     const [updateRows] = await conn.query(updateSQL, [updateData, study_id, apply_id]);
-    await conn.release();
     return updateRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
 const applyDelete = async (study_id, apply_id) => {
   try {
-    const conn = await pool.getConnection();
+    var conn = await pool.getConnection();
     const deleteSQL = 'DELETE FROM apply WHERE study_id = ? AND id = ?';
     const [deleteRows] = await conn.query(deleteSQL, [study_id, apply_id]);
-    await conn.release();
     return deleteRows;
   } catch (err) {
     throw { status: 500, message: err.sqlMessage };
+  } finally {
+    await conn.release();
   }
 };
 
