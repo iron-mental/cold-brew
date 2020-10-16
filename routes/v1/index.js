2b53b1ea1e27189126396f087189a2aff2aa0b73
cheol-95 <rkdcjf0122@gmail.com>
2020-10-16 15:03:05 +0900

    feat: firebase 라우팅

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var userRouter = require('./user');
+var firebaseRouter = require('./firebase');
 
 /* routing */
 router.use('/user', userRouter);
+router.use('/firebase', firebaseRouter);
 
 module.exports = router;
