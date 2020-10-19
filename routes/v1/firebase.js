6fcf62f562b44592a17082669c4a8d7f87d296d4
cheol-95 <rkdcjf0122@gmail.com>
2020-10-19 11:14:13 +0900

    refactor: 줄바꿈 수정

--- routes/v1/firebase.js
+++ routes/v1/firebase.js
@@ -6,19 +6,9 @@ const firebaseController = require('../../controllers/firebase');
 
 const router = express.Router();
 
-router.post(
-  '/email-verification/:id',
-  firebaseValid.emailVerification,
-  asyncWrap(firebaseController.emailVerification)
-);
+router.post('/email-verification/:id', firebaseValid.emailVerification, asyncWrap(firebaseController.emailVerification)); // 이메일 인증 요청
+router.get('/email-verification', firebaseValid.emailVerificationProcess, asyncWrap(firebaseController.emailVerificationProcess)); // 이메일 인증
 
-// DB 수정
-router.get(
-  '/email-verification',
-  firebaseValid.emailVerificationProcess,
-  asyncWrap(firebaseController.emailVerificationProcess)
-);
-
-router.post('/reset-password/:email', firebaseValid.resetPassword, asyncWrap(firebaseController.resetPassword));
+router.post('/reset-password/:email', firebaseValid.resetPassword, asyncWrap(firebaseController.resetPassword)); // 비밀번호 리셋 요청
 
 module.exports = router;
