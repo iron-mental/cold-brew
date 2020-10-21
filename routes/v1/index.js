3678686408c9b508349589b4936e343a6a8c0a3d
cheol-95 <rkdcjf0122@gmail.com>
2020-10-21 21:22:31 +0900

    feat: 채팅 뷰 생성

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -10,4 +10,9 @@ router.use('/user', userRouter);
 router.use('/study', studyRouter);
 router.use('/firebase', firebaseRouter);
 
+// 채팅 뷰 제공
+router.get('/chat', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 module.exports = router;
