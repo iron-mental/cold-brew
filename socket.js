43cb98c6c640e1c7f2dfb34539eaba13f908b726
cheol-95 <rkdcjf0122@gmail.com>
2020-10-23 21:40:37 +0900

    feat: mongoDB 적용

--- socket.js
+++ socket.js
@@ -1,37 +1,91 @@
-var socketio = require('socket.io');
+const socketio = require('socket.io');
+
+const User = require('./models/user');
+const Room = require('./models/room');
+const Chat = require('./models/chat');
 
 const listen = (server) => {
   io = socketio.listen(server); // 소켓서버 실행
-  terminal = io.of('/terminal'); // 네임스페이스 지정
+  terminal = io.of('terminal'); // 네임스페이스 지정
 
   // 유저 접속 시 트리거 -> 앱 부팅 시
   terminal.on('connection', (socket) => {
-    // 첨에 접속할때 클라이언트는 user_id, nickname전달, 서버는 socket.nickname을 변경하고 DB에 socket.id와 user_id를 매핑하여 저장한다
-    socket.on('signup', (data) => {
-      // data 정보 mongoDB에 저장
-      socket.nickname = data.nickname;
+    // test 메뉴얼 출력
+    guideMessage(terminal, socket.id);
+
+    // 연결되면 무조건 한번 날려야함
+    socket.on('signup', (userData) => {
+      // userData = {user_id, nickname} -> 구조분해 할당 테스트 후 적용
+      socket.user_id = userData.user_id;
+      socket.nickname = userData.nickname;
+
+      userData.socket_id = socket.id;
+      User.updateOne({ user_id: userData.user_id }, { userData }, { upsert: true }).exec();
     });
 
-    console.log('connected: ', socket.id);
-    terminal.to(socket.id).emit('message', '-- 테스트 명령어 목록 --');
-    terminal.to(socket.id).emit('message', '룸 입장: join/ (roomNumber)');
-    terminal.to(socket.id).emit('message', '룸 퇴장: leave/ (roomNumber)');
-    terminal.to(socket.id).emit('message', '메시지 전송: message/ (roomNumber) / (message)');
-
     // 클라이언트가 스터디 생성 or 스터디 참여 시 발생시키는 이벤트
-    socket.on('join', (data) => {
-      // data = { user_id: '유저 아이디', nickname: '닉네임' , room: study_id }
-      // DB에 저장 { user_id, nickname, room, socket.id, date }
-      socket.join(data.study_id); // join
-      terminal.to(data.room).emit('system-join', `${nickname}님이 입장했습니다`); // 해당 룸에만 메시지 전달
+    socket.on('join', (joinData) => {
+      // joinData = { room_number, room_name }
+      socket.join(joinData.room_number); // join
+      terminal.to(joinData.room_number).emit('system-join', `${nickname}님이 입장했습니다`);
+
+      Room.updateOne(
+        { room_number: joinData.room_number },
+        { $push: { members: { user_id: socket.user_id } } },
+        (err, docs) => {
+          // 쿼리에 걸린게 없을 때
+          if (docs.nModified === 0) {
+            joinData.members = [memberData];
+            Room.create(joinData);
+          }
+        }
+      );
+
+      Chat.create({
+        room_number: joinData.room_number,
+        user_id: 'system',
+        nickname: 'system-join',
+        message: `${socket.nickname}님이 입장했습니다`,
+        date: new Data().toString(),
+      });
+    });
+
+    // 클라이언트가 스터디 탈퇴, 회원 탈퇴시 발생시키는 이벤트
+    socket.on('leave', (leaveData) => {
+      // leaveData = { room_number, room_name }
+      socket.leave(leaveData.room_number); // join
+      terminal.to(leaveData.room_number).emit('system-leave', `${socket.nickname}님이 퇴장했습니다`);
+
+      Room.updateOne({ room_number: joinData.room_number }, { $pull: { members: { user_id: socket.user_id } } }).exec();
+
+      Chat.create({
+        room_number: leaveData.room_number,
+        user_id: 'system',
+        nickname: 'system-leave',
+        message: `${socket.nickname}님이 퇴장했습니다`,
+        date: new Data().toString(),
+      });
     });
 
     // 채팅 이벤트를 받았을 때 처리
-    socket.on('message', (msg) => {
-      const division = msg.indexOf('/');
-      const room = msg.slice(0, division);
-      const message = msg.slice(division + 1);
-      terminal.to(room).emit('message', message); // 해당 룸에만 메시지 전달
+    socket.on('message', (chatData) => {
+      // chatData = { room, msg }
+      terminal.to(chatData.room).emit('message', chatData.msg); // 해당 룸에만 메시지 전달
+
+      Chat.create({
+        room_number: chatData.room,
+        user_id: socket.user_id,
+        nickname: socket.nickname,
+        message: chatData.msg,
+        date: new Data().toString(),
+      });
+    });
+
+    // 유저 접속 해제 시 발생
+    socket.on('disconnect', () => {
+      console.log('disconnected: ', socket.id);
+      // user - socket.id 삭제
+      User.updateOne({ socket_id: socket.id }, { socket_id: '' }, (err, docs) => {});
     });
 
     // test command
@@ -47,8 +101,6 @@ const listen = (server) => {
         const message = contents.slice(division + 1);
         terminal.to(room).emit('message', socket.nickname.concat(': ', message));
       } else if (command === 'join') {
-        // console.log('terminal.clients(): ', terminal.clients());
-        // console.log('terminal.clients(1): ', terminal.clients('1'));
         socket.join(contents); // join
         terminal.to(contents).emit('message', `${socket.id}님이 입장했습니다`); // 해당 룸에만 메시지 전달
         const tt = socket.adapter.rooms;
@@ -66,27 +118,17 @@ const listen = (server) => {
         socket.disconnect(contents);
       }
     });
-
-    // 클라이언트가 스터디 탈퇴, 회원 탈퇴시 발생시키는 이벤트
-    socket.on('leave', (data) => {
-      // data = { user_id: '유저 아이디', nickname: '닉네임' , room: study_id }
-      // DB에서 삭제 { user_id, nickname, room, socket.id, date }
-      socket.leave(data.study_id); // join
-      terminal.to(data.room).emit('system-leave', `${nickname}님이 퇴장했습니다`); // 해당 룸에만 메시지 전달
-    });
-
-    // 유저 접속 해제 시 발생
-    socket.on('disconnect', () => {
-      console.log('disconnected: ', socket.id);
-      //가입했던 Room에 브로드캐스트를 통해 채팅방을 나갔다는 메시지 추가
-      // DB 삭제
-    });
   });
 
   return io;
 };
 
-// socket.broadcast.to('room1').emit('chat message', `system: ${socket.id}}님이 입장했습니다`); // socket의 주체를 제외하고 전달 (나 빼고)
-// terminal.to(room).emit('system', `${socket.id}}님이 입장했습니다`); // 해당 룸에만 메시지 전달
-
 module.exports = { listen };
+
+const guideMessage = (terminal, socket_id) => {
+  console.log(`new connection: ${socket_id}`);
+  terminal.to(socket_id).emit('message', '-- 테스트 명령어 목록 --');
+  terminal.to(socket_id).emit('message', '룸 입장: join/ (roomNumber)');
+  terminal.to(socket_id).emit('message', '룸 퇴장: leave/ (roomNumber)');
+  terminal.to(socket_id).emit('message', '메시지 전송: message/ (roomNumber) / (message)');
+};
