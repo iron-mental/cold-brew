8a1ad82a40376838ec41a79d812741a6d81c27da
cheol-95 <rkdcjf0122@gmail.com>
2020-10-29 23:22:39 +0900

    refactor: rowSplit 리팩토링

--- utils/database.js
+++ utils/database.js
@@ -1,97 +1,34 @@
-const rowSplit = (userData, tags = []) => {
-  let count = 0;
-  const items = {}; // make matching k-v
-  const result = {}; // 메인 저장소 result
-  const resultedId = {}; // 중복검사
+const rowSplit = (rows, tags) => {
+  // 결과, 키 목록, 임시 저장 모델, 저장된 id
+  let [result, keys, tmpModel, insertedId] = [{}, {}, {}, {}];
 
-  for (const tag of tags) {
-    items[tag] = tag.slice(0, 1).toUpperCase().concat('_');
+  // 사전 작업
+  tags.forEach((tag) => {
     result[tag] = [];
-    resultedId[tag] = [];
-  }
+    tmpModel[tag] = {};
+    insertedId[tag] = [];
+    keys[tag[0].toUpperCase()] = tag;
+  });
 
-  // rows FOR문
-  for (const data of userData) {
-    // 담을 변수 선언
-    let tmp = {};
-    for (let name in items) {
-      tmp[name] = {};
-    }
-
-    // row FOR문
-    for (const key in data) {
-      // 메인 데이터인지 확인
-      if (key.slice(0) === key.slice(0).toLowerCase() && count < 1) {
-        result[key] = data[key];
-      } else {
-        // items의 값과 매칭
-        for (let name in items) {
-          // if (key.slice(0, 2) === items[name] && data[key]) { // null을 안받는 구문, 아니면 null이면 데이터를 변환하는것을 넣을까?
-          if (key.slice(0, 2) === items[name]) {
-            tmp[name][key.slice(2)] = data[key];
-          }
-        }
+  // 메인 루프
+  rows.forEach((row, idx) => {
+    let tmp = tmpModel;
+    for (const [key, value] of Object.entries(row)) {
+      if (idx === 0 && !(key[0] in keys)) {
+        result[key] = row[key];
+      } else if (key[0] in keys) {
+        tmp[keys[key[0]]][key.slice(1)] = value;
       }
     }
 
-    //담은 변수들 result에 병합
-    for (const name in items) {
-      // 하위 row의 길이체크, 이미 들어간 내용과 동일한지, 해당 row의 id가 null이 아닌지 체크
-      if (Object.keys(tmp[name]).length && resultedId[name].indexOf(tmp[name].id) === -1 && tmp[name].id !== null) {
-        result[name].push(tmp[name]);
-        resultedId[name].push(tmp[name].id);
+    for (const [key, value] of Object.entries(tmp)) {
+      if (insertedId[key].indexOf(value.id) === -1 && Object.keys(value).length && value.id) {
+        insertedId[key].push(value.id);
+        result[key].push(Object.assign({}, value));
       }
     }
-    count += 1;
-  }
+  });
   return result;
 };
 
 module.exports = { rowSplit };
-
-/* 해당 유틸의 example
-const userData = [
-  {
-    id: 1,
-    name: 'kang',
-    P_id: 'pid',
-    P_name: 'pname',
-    T_id: 'tid',
-    T_name: 'tname',
-  },
-  {
-    id: 1,
-    name: 'kang',
-    P_id: 'pid2',
-    P_name: 'pname2',
-    T_id: 'tid2',
-    T_name: 'tname2',
-  },
-];
-const items = ['project', 'team'];
-const res = testFunction(userData, items);
-## 결과 ##
-result: { 
-  project:
-  [
-    { id: 'pid', name: 'pname' },
-    { id: 'pid2', name: 'pname2' }
-  ],
-  team:
-  [
-    { id: 'tid', name: 'tname' },
-    { id: 'tid2', name: 'tname2' }
-  ],
-  id: 1,
-  name: 'kang'
-*/
-
-// let count = 0;
-// const object1 = {
-//   a: 'somestring',
-//   b: 42,
-// };
-
-// for (const [key, value] of Object.entries(object1)) {
-//   console.log(`${key}: ${value}`);
-// }
