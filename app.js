06892bef5c44c78ff32ed4e22ae5dfb44072954e
cheol-95 <rkdcjf0122@gmail.com>
2020-11-02 01:21:08 +0900

    feat: 에러핸들러 적용

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const admin = require('firebase-admin');
 
 const v1Router = require('./routes/v1');
 const config = require('./configs/config');
-const response = require('./utils/response');
+const errorHandler = require('./utils/errors/customHandler');
 
 firebase.initializeApp(config.firebase);
 
@@ -32,22 +32,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-const fs = require('fs');
-// error handler
-app.use((err, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {});
-  }
-
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  if (!err.status) {
-    console.error(err);
-  }
-
-  response(res, err.message, err.status || 500);
-});
+// pre-error handler
+app.use(errorHandler);
 
 module.exports = app;
