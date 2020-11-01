a56775b22a176f79205f9f38b866f494a5982fc9
cheol-95 <rkdcjf0122@gmail.com>
2020-11-01 23:48:50 +0900

    fix: study 유효성검사 수정

--- utils/validators/study.js
+++ utils/validators/study.js
@@ -4,16 +4,17 @@ const createStudy = async (req, res, next) => {
   const bodySchema = Joi.object({
     user_id: Joi.number().required(),
     category: Joi.string().required(),
-    title: Joi.string().required(),
-    introduce: Joi.string().required(),
-    progress: Joi.string().required(),
-    study_time: Joi.string().required(),
+    title: Joi.string().required().min(2).max(10),
+    introduce: Joi.string().required().max(200),
+    progress: Joi.string().required().max(100),
+    study_time: Joi.string().required().max(100),
     location: Joi.string().required(),
     location_last: Joi.string().required(),
     location_detail: Joi.string().required(),
-    sns_notion: Joi.string(),
-    sns_evernote: Joi.string(),
-    sns_web: Joi.string(),
+    sns_notion: Joi.string().max(150),
+    sns_evernote: Joi.string().max(60),
+    sns_web: Joi.string().max(200),
+    image: Joi.allow(),
   });
   try {
     await bodySchema.validateAsync(req.body);
@@ -41,16 +42,17 @@ const studyUpdate = async (req, res, next) => {
   });
   const bodySchema = Joi.object({
     category: Joi.string(),
-    title: Joi.string(),
-    introduce: Joi.string(),
-    progress: Joi.string(),
-    study_time: Joi.string(),
+    title: Joi.string().min(2).max(10),
+    introduce: Joi.string().max(200),
+    progress: Joi.string().max(100),
+    study_time: Joi.string().max(100),
     location: Joi.string(),
     location_last: Joi.string(),
     location_detail: Joi.string(),
-    sns_notion: Joi.string(),
-    sns_evernote: Joi.string(),
-    sns_web: Joi.string(),
+    sns_notion: Joi.string().max(150),
+    sns_evernote: Joi.string().max(60),
+    sns_web: Joi.string().max(200),
+    image: Joi.allow(),
   }).min(1);
   try {
     await paramSchema.validateAsync(req.params);
