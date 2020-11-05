792afa55d29e0ee1e5ea0d846e5e069e0ca4fd29
cheol-95 <rkdcjf0122@gmail.com>
2020-11-05 23:55:50 +0900

    feat: jwt 검증 예외추가

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,26 +2,28 @@ const jwt = require('jsonwebtoken');
 
 const { authError } = require('../utils/errors/customError');
 
-const exception = ['check-nickname', 'check-email', 'login', 'reset-password'];
+const exception = ['check-nickname', 'check-email', 'login', 'reset-password', 'reissuance'];
 
 const verify = async (req, res, next) => {
+  const path = req.url.split('/')[3];
+
+  // 토큰이 없어도 되는 APIs 확인
+  if (exception.indexOf(path) > -1 || req.url === '/v1/user') {
+    return next();
+  }
   // 토큰 유무 확인
-  if (req.headers.authorization) {
+  else if (req.headers.authorization) {
     try {
       const decoded = await jwt.verify(req.headers.authorization.split(' ')[1], process.env.JWT_secret);
       res.user = decoded;
-      next();
+      return next();
     } catch (err) {
-      authError(next, err);
+      return authError(next, err);
     }
-  }
-  // 토큰이 없어도 되는 APIs 확인
-  else if (exception.indexOf(req.url.split('/')[3]) > -1 || req.url === '/v1/user') {
-    next();
   }
   // 나머지 에러처리
   else {
-    authError(next, { message: 'jwt not exist' });
+    return authError(next, { message: 'jwt not exist' });
   }
 };
 
