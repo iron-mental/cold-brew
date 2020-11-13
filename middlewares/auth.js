27454fba8624e76a95e2a1955fd3580d52fb0773
cheol-95 <rkdcjf0122@gmail.com>
2020-11-13 15:44:26 +0900

    feat: 토큰검증 -  Access Token 재발급 예외처리

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,28 +2,22 @@ const jwt = require('jsonwebtoken');
 
 const { authError } = require('../utils/errors/customError');
 
-const exception = ['check-nickname', 'check-email', 'login', 'reset-password'];
+const exception = ['check-nickname', 'check-email', 'login', 'reset-password', 'reissuance'];
 
 const verify = async (req, res, next) => {
-  // 토큰이 없어도 되는 APIs 확인
   if (exception.indexOf(req.url.split('/')[3]) > -1 || req.url === '/v1/user') {
     return next();
   }
-  // 토큰 유무 확인
-  else if (req.headers.authorization) {
+  if (req.headers.authorization) {
     req.jwt = req.headers.authorization.split(' ')[1];
     try {
-      const decoded = await jwt.verify(req.jwt, process.env.JWT_secret);
-      req.user = decoded;
+      req.user = await jwt.verify(req.jwt, process.env.JWT_secret);
       return next();
     } catch (err) {
       return authError(next, err);
     }
   }
-  // 나머지 에러처리
-  else {
-    return authError(next, { message: 'jwt not exist' });
-  }
+  return authError(next, { message: 'jwt not exist' });
 };
 
 module.exports = {
