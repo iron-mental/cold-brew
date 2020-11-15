a1081acb96b10a287e2239121c973a5ba709a624
cheol-95 <rkdcjf0122@gmail.com>
2020-11-15 17:52:51 +0900

    refactor: 환경변수 적용

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,31 +1,27 @@
 const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.JWT_secret;
-const issuer = process.env.JWT_issuer;
+
+const options = {
+  issuer: process.env.JWT_issuer,
+};
 
 const getAccessToken = ({ id, email }) => {
-  const data = {
-    aud: id,
-    email,
-  };
-  const options = {
-    expiresIn: 60 * 15, // 15분
-    subject: 'userInfo-access',
-    issuer,
-  };
-  return jwt.sign(data, secretKey, options);
+  const payload = { id, email };
+
+  options.expiresIn = parseInt(process.env.JWT_accessExpire, 10); // 15분
+  options.subject = 'userInfo-access';
+
+  return jwt.sign(payload, secretKey, options);
 };
 
 const getRefreshToken = ({ id }) => {
-  const data = {
-    aud: id,
-  };
-  const options = {
-    expiresIn: '15d', // 15일
-    subject: 'userInfo-refresh',
-    issuer,
-  };
-  return jwt.sign(data, secretKey, options);
+  const payload = { id };
+
+  options.expiresIn = process.env.JWT_refreshExpire; // 15일
+  options.subject = 'userInfo-refresh';
+
+  return jwt.sign(payload, secretKey, options);
 };
 
 module.exports = {
