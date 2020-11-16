2f51812740f0ce35b8cd383f947d4e8bf9be52dd
cheol-95 <rkdcjf0122@gmail.com>
2020-11-16 15:54:11 +0900

    feat: AuthEnum 추가, 지정 권한 확인 구현

--- services/common.js
+++ services/common.js
@@ -1,11 +1,17 @@
 const commonDao = require('../dao/common');
+const { customError } = require('../utils/errors/customError');
 
-const statusEnum = Object.freeze({
+const authEnum = Object.freeze({
   host: 1,
   member: 2,
   apply: 3,
   rejected: 4,
   none: 5,
+  1: 'host',
+  2: 'member',
+  3: 'apply',
+  4: 'rejected',
+  5: 'none',
 });
 
 const isHost = async ({ id: user_id }, { study_id }) => {
@@ -15,28 +21,37 @@ const isHost = async ({ id: user_id }, { study_id }) => {
   }
 };
 
-const checkMember = async ({ id: user_id }, { study_id }) => {
+const checkAuth = async ({ id: user_id }, { study_id }) => {
   const memberCheckRows = await commonDao.checkMember(user_id, study_id);
   if (memberCheckRows.length === 1) {
     if (memberCheckRows[0].leader === 1) {
-      return statusEnum.host;
+      return authEnum.host;
     } else {
-      return statusEnum.member;
+      return authEnum.member;
     }
   }
 
   const applyCheckRows = await commonDao.checkApply(user_id, study_id);
   if (applyCheckRows.length === 1) {
     if (applyCheckRows[0].rejected_status === 0) {
-      return statusEnum.apply;
+      return authEnum.apply;
     } else {
-      return statusEnum.rejected;
+      return authEnum.rejected;
     }
   }
-  return statusEnum.none;
+  return authEnum.none;
+};
+
+const checkAuthority = async ({ id: user_id }, { study_id }, ...levels) => {
+  const statusNumber = await checkAuth({ id: user_id }, { study_id });
+  const status = authEnum[statusNumber];
+  if (levels.indexOf(status) === -1) {
+    throw customError(401, '권한이 없습니다');
+  }
 };
 
 module.exports = {
   isHost,
-  checkMember,
+  checkAuth,
+  checkAuthority,
 };
