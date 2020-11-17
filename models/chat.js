42cd055cff3bcd95ddb342a05245b8840a182563
cheol-95 <rkdcjf0122@gmail.com>
2020-11-17 23:32:57 +0900

    feat: Chat 모델 - date 타입, versionKey 제거

--- models/chat.js
+++ models/chat.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const chatSchema = new mongoose.Schema({
-  room_number: String, // study_id
-  user_id: String, // 유저 아이디
-  nickname: String, // 유저 닉네임
-  message: String, // 내용
-  date: String, // 시간 -> node에서 생성
-});
+const chatSchema = new mongoose.Schema(
+  {
+    room_number: String, // study_id
+    user_id: String, // 유저 아이디
+    nickname: String, // 유저 닉네임
+    message: String, // 내용
+    date: String, // unixtime
+  },
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = mongoose.model('Chat', chatSchema);
