e886724aabff0ba2ca941a7aa7763723aae76807
cheol-95 <rkdcjf0122@gmail.com>
2020-11-17 23:34:06 +0900

    feat: User 모델 - 참여한 rooms, disConnected_at 추가

--- models/user.js
+++ models/user.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  user_id: String, // user_id
-  nickname: String, // nickname
-  socket_id: String, // socket.id
-});
+const userSchema = new mongoose.Schema(
+  {
+    user_id: String, // user_id
+    nickname: String, // nickname
+    socket_id: String, // socket.id
+    rooms: Array, // 가입한 Room 목록
+    disconnected_at: String, // unixtime
+  },
+  {
+    versionKey: false,
+  }
+);
 
 module.exports = mongoose.model('User', userSchema);
