1922e9b7f37f6f20edcbcb2b1b94273d406a3d7a
cheol-95 <rkdcjf0122@gmail.com>
2020-11-19 19:15:12 +0900

    feat: Enum 적용

--- services/common.js
+++ services/common.js
@@ -1,18 +1,6 @@
 const commonDao = require('../dao/common');
 const { customError } = require('../utils/errors/customError');
-
-const authEnum = Object.freeze({
-  host: 1,
-  member: 2,
-  apply: 3,
-  rejected: 4,
-  none: 5,
-  1: 'host',
-  2: 'member',
-  3: 'apply',
-  4: 'rejected',
-  5: 'none',
-});
+const { authEnum } = require('../utils/variables/enums');
 
 const isHost = async ({ id: user_id }, { study_id }) => {
   const checkRows = await commonDao.isHost(user_id, study_id);
@@ -24,28 +12,20 @@ const isHost = async ({ id: user_id }, { study_id }) => {
 const checkAuth = async ({ id: user_id }, { study_id }) => {
   const memberCheckRows = await commonDao.checkMember(user_id, study_id);
   if (memberCheckRows.length === 1) {
-    if (memberCheckRows[0].leader === 1) {
-      return authEnum.host;
-    } else {
-      return authEnum.member;
-    }
+    return authEnum[memberCheckRows[0].leader === 1 ? 'host' : 'member'];
   }
 
   const applyCheckRows = await commonDao.checkApply(user_id, study_id);
   if (applyCheckRows.length === 1) {
-    if (applyCheckRows[0].rejected_status === 0) {
-      return authEnum.apply;
-    } else {
-      return authEnum.rejected;
-    }
+    return authEnum[applyCheckRows[0].rejected_status === 0 ? 'applicant' : 'rejected'];
   }
+
   return authEnum.none;
 };
 
-const checkAuthority = async ({ id: user_id }, { study_id }, ...levels) => {
-  const statusNumber = await checkAuth({ id: user_id }, { study_id });
-  const status = authEnum[statusNumber];
-  if (levels.indexOf(status) === -1) {
+const checkAuthority = async ({ id: user_id }, { study_id }, ...authority) => {
+  const status = await checkAuth({ id: user_id }, { study_id });
+  if (authority.indexOf(status) === -1) {
     throw customError(401, '권한이 없습니다');
   }
 };
