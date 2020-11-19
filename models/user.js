dc279eb74597fa5b35bf068d628b14a872340558
cheol-95 <rkdcjf0122@gmail.com>
2020-11-19 23:01:30 +0900

    fix: 유저 모델 - String -> Number

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    user_id: String, // user_id
+    user_id: Number, // user_id
     nickname: String, // nickname
     socket_id: String, // socket.id
     rooms: Array, // 가입한 Room 목록
