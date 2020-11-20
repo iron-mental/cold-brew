82125eb5a0df15a95e02e07e382d8c4a3b049f42
cheol-95 <rkdcjf0122@gmail.com>
2020-11-20 17:28:35 +0900

    fix: Room, Chat 모델 - 타입 변경

--- models/chat.js
+++ models/chat.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema(
   {
-    room_number: String, // study_id
-    user_id: String, // 유저 아이디
+    room_number: Number, // study_id
+    user_id: Number, // 유저 아이디
     nickname: String, // 유저 닉네임
     message: String, // 내용
     date: String, // unixtime
--- models/room.js
+++ models/room.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema(
   {
-    room_number: String, // study_id
+    room_number: Number, // study_id
     room_name: String, // 스터디 이름
     members: Array, // 각 원소는 object형태 { user_id, socket_id }
     off_members: Array, // 소켓연결이 끊겨있는 user_id만 저장
