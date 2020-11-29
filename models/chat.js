9889d46e1017bccc4a168c62255e418aac9f1027
cheol-95 <rkdcjf0122@gmail.com>
2020-11-29 12:51:22 +0900

    fix: mongoDB 스키마 변경

--- models/chat.js
+++ models/chat.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema(
   {
-    room_number: Number, // study_id
     user_id: Number, // 유저 아이디
+    study_id: Number, // study_id
     nickname: String, // 유저 닉네임
     message: String, // 내용
     date: String, // unixtime
