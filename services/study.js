ecbf25986cba569476ae2df7549333e010fd4a6d
cheol-95 <rkdcjf0122@gmail.com>
2020-11-29 23:45:02 +0900

    feat: 스터디 탈퇴관련 api

--- services/study.js
+++ services/study.js
@@ -5,6 +5,7 @@ const studyDao = require('../dao/study');
 const { getUserLocation } = require('../dao/common');
 const { rowSplit, toBoolean, locationMerge, cutId, customSorting } = require('../utils/query');
 const { customError } = require('../utils/errors/customError');
+const { authEnum } = require('../utils/variables/enums');
 
 const User = require('../models/user');
 const Room = require('../models/room');
@@ -17,11 +18,11 @@ const createStudy = async ({ id: user_id }, createData) => {
   const createRows = await studyDao.createStudy(user_id, createData);
 
   Room.create({
-    room_number: createRows.insertId,
-    room_name: createData.title,
+    study_id: createRows.insertId,
+    study_title: createData.title,
     members: [user_id],
-  });
-  User.updateOne({ user_id }, { $push: { rooms: createRows.insertId } }, { upsert: true }, () => {});
+  }).exec();
+  User.updateOne({ user_id }, { $push: { rooms: createRows.insertId } }, { upsert: true }).exec();
 
   return createRows.insertId;
 };
@@ -66,6 +67,17 @@ const studyUpdate = async ({ study_id }, updateData, filedata) => {
   }
 };
 
+const studyDelete = async ({ study_id }) => {
+  const studyRows = await studyDao.studyDelete(study_id);
+  if (studyRows.affectedRows === 0) {
+    throw customError(400, '스터디 삭제 실패');
+  }
+  Room.remove({ study_id }).exec();
+  User.updateOne({ user_id }, { $pull: { rooms: study_id } }).exec();
+
+  // 멤버에게 채팅 or 노티 전송부
+};
+
 const myStudy = async ({ id }) => {
   const myStudyList = await studyDao.getMyStudy(id);
   if (myStudyList.length === 0) {
@@ -97,11 +109,48 @@ const studyPaging = async (studyKeys) => {
   return await studyDao.studyPaging(studyKeys);
 };
 
+const leaveStudy = async ({ id }, { study_id }, authority) => {
+  if (authority === authEnum.host) {
+    const participateRows = await studyDao.getStudy(study_id);
+    if (participateRows.length > 1) {
+      throw customError(400, '탈퇴할 수 없습니다. 스터디 장을 위임한 뒤 탈퇴하세요.');
+    }
+    const studyRows = await studyDao.studyDelete(study_id);
+    if (studyRows.affectedRows === 0) {
+      throw customError(400, '스터디 탈퇴 실패');
+    }
+  }
+
+  const { applyRows, participateRows } = await studyDao.leaveStudy(id, study_id);
+  if (applyRows.affectedRows === 0 || participateRows.affectedRows === 0) {
+    throw customError(400, '스터디 탈퇴 실패');
+  }
+
+  setTimeout(() => {
+    Room.updateOne({ study_id }, { $pull: { off_members: id, members: id } });
+    User.updateOne({ user_id: id }, { $pull: { rooms: study_id } }).exec();
+  }, 1000);
+
+  // 멤버에게 채팅 or 노티 전송부
+};
+
+const delegate = async ({ id: old_leader }, { study_id }, { new_leader }) => {
+  const { toLeaderRows, toParticipateRows } = await studyDao.delegate(study_id, old_leader, new_leader);
+  if (toLeaderRows.affectedRows === 0 || toParticipateRows.affectedRows === 0) {
+    throw customError(400, '위임 실패');
+  }
+
+  // 멤버에게 채팅 or 노티 전송부
+};
+
 module.exports = {
   createStudy,
   studyDetail,
   studyUpdate,
+  studyDelete,
   myStudy,
   studyList,
   studyPaging,
+  leaveStudy,
+  delegate,
 };
