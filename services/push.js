cfde7f0775e4305736ef79b369fe0ac826b3d50b
cheol-95 <rkdcjf0122@gmail.com>
2020-12-01 20:48:57 +0900

    feat: getNote 생성

--- services/push.js
+++ services/push.js
@@ -5,36 +5,36 @@ const { apn: options } = require('../configs/config');
 
 const apnProvider = new apn.Provider(options);
 
-const note = new apn.Notification({
-  topic: process.env.APNS_bundleId,
-  sound: 'default',
-});
-
 /*
   FCM 초기화
 */
-
-const send = async (off_members, chat) => {
-  note.pay = chat;
-  note.expiry = Math.floor(Date.now() / 1000) + 3600;
-  note.alert = chat.nickname === '__SYSTEM__' ? chat.message : chat.nickname.concat(' ', chat.message);
-
-  const pushInfoRows = await pushDao.getPushInfo(off_members);
+const send = async (userList, chat) => {
+  const pushInfoRows = await pushDao.getPushInfo(userList);
 
   for (row of pushInfoRows) {
     if (row.device === 'ios') {
-      // note.badge = row.badge;
-      note.badge = 50;
-      await apnProvider.send(note, row.push_token).then((result) => {
+      apnProvider.send(getNote(chat, row.badge), row.push_token).then((result) => {
         if (result.failed.length > 0) {
           // apns 에러
         }
-        console.log('성공! result: ', result);
+        console.log('결과: ', result);
       });
     } else {
       // fcm push
     }
   }
+};
+
+const getNote = (chat, badge) => {
+  return new apn.Notification({
+    topic: process.env.APNS_bundleId,
+    sound: 'default',
+    badge: 50,
+    // badge,
+    pay: chat,
+    expiry: Math.floor(Date.now() / 1000) + 3600,
+    alert: chat.nickname === '__SYSTEM__' ? chat.message : chat.nickname.concat(' ', chat.message),
+  });
 };
 
 module.exports = { send };
