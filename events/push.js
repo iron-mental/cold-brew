40f026187ca72f818aa25fca83e1f8262064806f
cheol-95 <rkdcjf0122@gmail.com>
2020-12-01 20:47:41 +0900

    feat: fullArray 모듈화

--- events/push.js
+++ events/push.js
@@ -5,21 +5,26 @@ const pushService = require('../services/push');
 
 const push = new EventEmitter();
 
-push.on('send-offMembers', async (study_id, chat) => {
+push.on('send-offMembers', async (study_id, chat, withTitle) => {
   const { off_members } = await Room.findOne({ study_id });
   const length = off_members.length;
-
   if (length > 0) {
-    for (i = 0; i < 10 - length; i++) {
-      off_members.push(undefined);
-    }
-    pushService.send(off_members, chat);
+    pushService.send(fullArray(off_members), chat);
   }
 });
 
-push.on('system-notification', (study_id, chat) => {
-  console.log('study_id, chat: ', study_id, chat);
-  pushService.send(study_id, chat);
+// 참여, 탈퇴를 제외한 알람 발생 시 트리거
+push.on('send-members', async (study_id, chat) => {
+  const { members } = await Room.findOne({ study_id });
+  // pushService.send(fullArray(members), chat);
 });
+
+const fullArray = (target) => {
+  const length = target.length;
+  for (i = 0; i < 10 - length; i++) {
+    target.push(null);
+  }
+  return target;
+};
 
 module.exports = push;
