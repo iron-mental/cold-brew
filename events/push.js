f8ef0af7b6ec15d5edd8ccf65395cc0bb8a97db6
cheol-95 <rkdcjf0122@gmail.com>
2020-12-01 19:21:56 +0900

    feat: push 이벤트 생성

--- events/push.js
+++ events/push.js
@@ -1,6 +1,25 @@
-const broadcast = require('./broadcast');
+const { EventEmitter } = require('events');
+
+const Room = require('../models/room');
 const pushService = require('../services/push');
 
-broadcast.on('participate', (room_number, message) => {
-  //push 실행부
+const push = new EventEmitter();
+
+push.on('send-offMembers', async (study_id, chat) => {
+  const { off_members } = await Room.findOne({ study_id });
+  const length = off_members.length;
+
+  if (length > 0) {
+    for (i = 0; i < 10 - length; i++) {
+      off_members.push(undefined);
+    }
+    pushService.send(off_members, chat);
+  }
 });
+
+push.on('system-notification', (study_id, chat) => {
+  console.log('study_id, chat: ', study_id, chat);
+  pushService.send(study_id, chat);
+});
+
+module.exports = push;
