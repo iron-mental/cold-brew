23acef87be5d81b63a0a193251fa22d9bdff5c23
cheol-95 <rkdcjf0122@gmail.com>
2020-12-08 21:09:40 +0900

    feat: database 에러추가

--- utils/errors/database.js
+++ utils/errors/database.js
@@ -8,9 +8,16 @@ class DatabaseError extends Error {
 }
 
 const databaseError = (err) => {
-  return new DatabaseError(err);
-};
+  if (err.errno === 1451) {
+    err.sqlMessage = '해당 사용자에게 종속되어있는 데이터가 존재합니다';
+    return new DatabaseError(err);
+  }
 
+  if (err.errno === 1452) {
+    err.sqlMessage = '조회된 사용자가 없습니다.';
+    return new DatabaseError(err);
+  }
+};
 module.exports = {
   DatabaseError,
   databaseError,
