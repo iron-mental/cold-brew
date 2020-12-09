697c8ad79a9c56919959118b5cbe34c0e58eecb4
cheol-95 <rkdcjf0122@gmail.com>
2020-12-09 15:43:11 +0900

    feat: verify 유틸 추가

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,6 +5,14 @@ const options = {
   issuer: process.env.JWT_issuer,
 };
 
+const verify = (token, name) => {
+  try {
+    return jwt.verify(token, process.env.JWT_secret);
+  } catch (err) {
+    throw customError(401, `${name} Token이 만료되었습니다. 다시 로그인 하세요.`);
+  }
+};
+
 const getAccessToken = ({ id, email, nickname }) => {
   const payload = { id, email, nickname };
   options.expiresIn = parseInt(process.env.JWT_accessExpire, 10); // 15분
@@ -20,6 +28,7 @@ const getRefreshToken = ({ id }) => {
 };
 
 module.exports = {
+  verify,
   getAccessToken,
   getRefreshToken,
 };
