f28bdcb30542e8445949db0dfe72f73afd5021de
cheol-95 <rkdcjf0122@gmail.com>
2020-12-10 21:20:26 +0900

    refactor: require 수정

--- dao/push.js
+++ dao/push.js
@@ -1,5 +1,5 @@
 const pool = require('./db');
-const { customError } = require('../utils/errors/customError');
+const { customError } = require('../utils/errors/custom');
 
 const getPushInfo = async (members) => {
   const conn = await pool.getConnection();
