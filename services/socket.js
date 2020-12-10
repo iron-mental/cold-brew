12c17495bb8351413bb504fd70e5f05abd1ca4a3
cheol-95 <rkdcjf0122@gmail.com>
2020-12-10 18:08:58 +0900

    refactor: 소켓 로직분리, chat 제외한 mognoDB 제거

--- services/socket.js
+++ services/socket.js
@@ -1,18 +1,10 @@
 const socketDao = require('../dao/socket');
 const push = require('../events/push');
 
-const User = require('../models/user');
-const Room = require('../models/room');
 const Chat = require('../models/chat');
-
-const getHexTimestamp = () => {
-  return Math.floor(new Date().getTime() / 1000).toString(16) + '0000000000000000';
-};
 
 const connection = (study_id, user_id, socket_id, nickname) => {
   socketDao.updateChatStatus(true, study_id, user_id);
-  User.updateOne({ user_id }, { socket_id, nickname }).exec();
-  Room.updateOne({ study_id }, { $pull: { off_members: user_id } }).exec();
 };
 
 const chat = (terminal, study_id, nickname, message) => {
@@ -20,13 +12,11 @@ const chat = (terminal, study_id, nickname, message) => {
   terminal.to(study_id).emit('message', JSON.stringify(userChat));
   Chat.create(userChat);
 
-  push.emit('offMembers', study_id, userChat, nickname);
+  push.emit('chat', study_id, userChat, nickname);
 };
 
 const disconnection = (study_id, user_id) => {
   socketDao.updateChatStatus(false, study_id, user_id);
-  User.updateOne({ user_id }, { disconnected_at: getHexTimestamp() }).exec();
-  Room.updateOne({ study_id }, { $addToSet: { off_members: user_id } }).exec();
 };
 
 module.exports = {
