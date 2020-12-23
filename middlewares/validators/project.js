181e5a1fcfc043de5587852e48a5833190bd042d
cheol-95 <rkdcjf0122@gmail.com>
2020-12-23 23:53:19 +0900

    feat: 프로젝트 유효성검사 - 배열검사 구현

--- middlewares/validators/project.js
+++ middlewares/validators/project.js
@@ -51,18 +51,21 @@ const deleteProject = async (req, res, next) => {
 const updateProject = async (req, res, next) => {
   const paramSchema = Joi.object({
     id: Joi.number().required(),
-    project_id: Joi.number().required(),
   });
-  const bodySchema = Joi.object({
+
+  const projectSchema = Joi.object({
+    id: Joi.number(),
     title: Joi.string().max(20),
     contents: Joi.string().max(200),
     sns_github: Joi.string().allow('').max(40),
     sns_appstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
     sns_playstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
   });
+
+  const projects = Joi.array().items(projectSchema).min(1);
   try {
     await paramSchema.validateAsync(req.params);
-    await bodySchema.validateAsync(req.body);
+    await projects.validateAsync(req.body);
     next();
   } catch (err) {
     next(validError(err));
