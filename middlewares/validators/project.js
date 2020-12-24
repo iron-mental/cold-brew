15dccb44b295c2b30a2a419c38a80c84407360ea
cheol-95 <rkdcjf0122@gmail.com>
2020-12-24 17:26:33 +0900

    fix: 프로젝트 유효성검사 - create, delete 제거

--- middlewares/validators/project.js
+++ middlewares/validators/project.js
@@ -3,42 +3,9 @@ const Joi = require('joi');
 const { validError } = require('../../utils/errors/validation');
 const commonValid = require('./common');
 
-const createProject = async (req, res, next) => {
-  const paramSchema = Joi.object({
-    id: Joi.number().required(),
-  });
-  const bodySchema = Joi.object({
-    title: Joi.string().max(20).required(),
-    contents: Joi.string().max(200).required(),
-    sns_github: Joi.string().allow('').max(40),
-    sns_appstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
-    sns_playstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
-  });
-  try {
-    await paramSchema.validateAsync(req.params);
-    await bodySchema.validateAsync(req.body);
-    next();
-  } catch (err) {
-    next(validError(err));
-  }
-};
-
 const getProjectList = async (req, res, next) => {
   const paramSchema = Joi.object({
     id: Joi.number().required(),
-  });
-  try {
-    await paramSchema.validateAsync(req.params);
-    next();
-  } catch (err) {
-    next(validError(err));
-  }
-};
-
-const deleteProject = async (req, res, next) => {
-  const paramSchema = Joi.object({
-    id: Joi.number().required(),
-    project_id: Joi.number().required(),
   });
   try {
     await paramSchema.validateAsync(req.params);
@@ -55,8 +22,8 @@ const updateProject = async (req, res, next) => {
 
   const projectSchema = Joi.object({
     id: Joi.number(),
-    title: Joi.string().max(20),
-    contents: Joi.string().max(200),
+    title: Joi.string().max(20).required(),
+    contents: Joi.string().max(200).required(),
     sns_github: Joi.string().allow('').max(40),
     sns_appstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
     sns_playstore: Joi.string().allow('').custom(commonValid.uriMethod).max(150),
@@ -73,8 +40,6 @@ const updateProject = async (req, res, next) => {
 };
 
 module.exports = {
-  createProject,
   getProjectList,
   updateProject,
-  deleteProject,
 };
