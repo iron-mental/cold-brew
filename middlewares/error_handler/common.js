eddd4f32ba986fa49f2cf16b5430b73cb0075967
cheol-95 <rkdcjf0122@gmail.com>
2021-01-11 14:04:16 +0900

    refactor: 로깅 분리

---
+++ middlewares/error_handler/common.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { format } = require('date-fns');
+const { logger } = require('../../configs/winston');
+
+const commonErrorHandler = (err, req, res, next) => {
+  const errObj = {
+    req: {
+      headers: req.headers,
+      query: req.query,
+      body: req.body,
+      route: req.route,
+    },
+    error: {
+      message: err.message,
+      stack: err.stack,
+      status: err.status,
+    },
+    user: req.user,
+  };
+  logger.error(`${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`, errObj);
+
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {});
+  }
+
+  next(err);
+};
+
+module.exports = {
+  commonErrorHandler,
+};
--- middlewares/error_handler/index.js
+++ middlewares/error_handler/index.js
@@ -1,44 +1,9 @@
-const fs = require('fs');
-
 const { authErrorHandler } = require('./auth');
 const { validErrorHandler } = require('./validation');
 const { firebaseErrorHandler } = require('./firebase');
 const { databaseErrorHandler } = require('./database');
 const { customErrorHandler } = require('./custom');
-const { logger } = require('../../configs/winston');
-
-const commonErrorHandler = (err, req, res, next) => {
-  try {
-    if (req.file) {
-      fs.unlink(req.file.path, (err) => {});
-    }
-
-    const errObj = {
-      req: {
-        headers: req.headers,
-        query: req.query,
-        body: req.body,
-        route: req.route,
-      },
-      error: {
-        message: err.message,
-        stack: err.stack,
-        status: err.status,
-      },
-      user: req.user,
-    };
-
-    // logger.error(`${moment().format('YYYY-MM-DD HH:mm:ss')}`, errObj);
-    logger.error(errObj);
-
-    if (req.file) {
-      fs.unlink(req.file.path, (error) => {});
-    }
-    next(err);
-  } catch (err) {
-    console.error(err);
-  }
-};
+const { commonErrorHandler } = require('./common');
 
 module.exports = (app) => {
   app.use(commonErrorHandler);
