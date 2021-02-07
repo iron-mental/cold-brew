ee4ca47714e67f90a116c7d81ac3a8ed078a1be6
cheol-95 <rkdcjf0122@gmail.com>
2021-02-08 03:18:53 +0900

    feat: 이메일 인증 - 실패 에러처리

--- middlewares/error_handler/custom.js
+++ middlewares/error_handler/custom.js
@@ -1,7 +1,12 @@
 const { CustomError } = require('../../utils/errors/custom');
+const { makeAlert } = require('../../utils/makeAlert');
 
 const customErrorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
+    if (err.code > 200) {
+      return res.status(err.status).send(makeAlert(err.message));
+    }
+
     const status = err.status;
     delete err.status;
     return res.status(status).json(err);
