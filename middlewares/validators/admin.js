1b720a3dce18daacc8c6fb16d381a34adad9cfa9
cheol-95 <rkdcjf0122@gmail.com>
2021-02-10 13:01:29 +0900

    feat: admin - 유효성검사 추가

--- middlewares/validators/admin.js
+++ middlewares/validators/admin.js
@@ -1,2 +1,33 @@
 const Joi = require('joi');
+
 const { validError } = require('../../utils/errors/validation');
+
+const getRedis = async (req, res, next) => {
+  const paramSchema = Joi.object({
+    user_id: Joi.number().required(),
+  });
+  try {
+    await paramSchema.validateAsync(req.params);
+    next();
+  } catch (err) {
+    next(validError(err));
+  }
+};
+
+const setVersion = async (req, res, next) => {
+  const bodySchema = Joi.object({
+    version: Joi.string().required(),
+    force: Joi.bool().required(),
+  });
+  try {
+    await bodySchema.validateAsync(req.body);
+    next();
+  } catch (err) {
+    next(validError(err));
+  }
+};
+
+module.exports = {
+  getRedis,
+  setVersion,
+};
