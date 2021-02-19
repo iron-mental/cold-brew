31f1272374fdceaf050981799b872b692c8c675e
cheol-95 <rkdcjf0122@gmail.com>
2021-02-19 18:15:57 +0900

    feat: admin 버전설정 - device 추가

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const v1Router = require('./v1');
 const commonRouter = require('./common');
+const adminRouter = require('./admin');
 
 const router = express.Router();
 
 /* routing */
 router.use('/v1', v1Router);
 router.use('/', commonRouter);
+router.use('/admin', adminRouter);
 
 // 채팅 뷰 제공
 router.get('/chat/https', (req, res, next) => {
