d6f5cbbec39cd329a77425fe6bcf659bc7f65193
cheol-95 <rkdcjf0122@gmail.com>
2021-02-21 02:26:54 +0900

    fix: push - toStudy 제거

--- events/push.js
+++ events/push.js
@@ -9,16 +9,12 @@ push.on('chat', (study_id, chat) => {
   pushService.chat(study_id, chat);
 });
 
-push.on('toUser', (pushEvent, user_id, study_id) => {
+push.on('toUser', (pushEvent, study_id, user_id) => {
   pushService.toUser(pushEvent, user_id, study_id);
 });
 
 push.on('toHost', (pushEvent, study_id) => {
   pushService.toHost(pushEvent, study_id);
-});
-
-push.on('toStudy', (pushEvent, study_id) => {
-  pushService.toStudy(pushEvent, study_id);
 });
 
 push.on('toStudyWithoutHost', (pushEvent, study_id) => {
