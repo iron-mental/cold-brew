12b4fdb4f734d379a096bff18da68203c5e5c9fb
cheol-95 <rkdcjf0122@gmail.com>
2021-02-21 02:27:18 +0900

    refactor: 푸시 구조 리팩토링

--- services/push.js
+++ services/push.js
@@ -1,39 +1,15 @@
 const admin = require('firebase-admin');
 const apn = require('apn');
 
+const pushDao = require('../dao/push');
+const studyDao = require('../dao/study');
 const { apn: options } = require('../configs/config');
 const { getChatPayload, getPushPayload } = require('../models/push');
-const pushDao = require('../dao/push');
 const { customError } = require('../utils/errors/custom');
 const { RedisEventEnum, PushEventEnum, DeviceEnum } = require('../utils/variables/enum');
 const { redisTrigger } = require('./redis');
 
 const apnProvider = new apn.Provider(options);
-
-const toHost = async (pushEvent, study_id) => {
-  const tokenRows = await pushDao.getHostToken(study_id);
-  send(tokenRows, pushEvent, study_id);
-};
-
-const toUser = async (pushEvent, user_id, study_id) => {
-  const tokenRows = await pushDao.getUserToken(user_id);
-  send(tokenRows, pushEvent, study_id);
-};
-
-const toStudy = async (pushEvent, study_id) => {
-  const tokenRows = await pushDao.getMemberToken(study_id);
-  send(tokenRows, pushEvent, study_id);
-};
-
-const toStudyWithoutHost = async (pushEvent, study_id) => {
-  const tokenRows = await pushDao.getMemberWithoutHostToken(study_id);
-  send(tokenRows, pushEvent, study_id);
-};
-
-const toStudyWithoutUser = async (pushEvent, study_id, user_id) => {
-  const tokenRows = await pushDao.getMemberWithoutUserToken(study_id, user_id);
-  send(tokenRows, pushEvent, study_id);
-};
 
 const chat = async (study_id, chat) => {
   const { apnsPayload, fcmPayload } = getChatPayload(chat);
@@ -54,11 +30,67 @@ const chat = async (study_id, chat) => {
   }
 };
 
-const send = async (tokenRows, pushEvent, study_id) => {
+// // 여기서부터 묶을거
+// const toHost = async (pushEvent, study_id) => {
+//   const tokenRows = await pushDao.getHostToken(study_id);
+//   send(tokenRows, pushEvent, study_id);
+// };
+
+// const toUser = async (pushEvent, study_id, user_id) => {
+//   const tokenRows = await pushDao.getUserToken(user_id);
+//   send(tokenRows, pushEvent, study_id);
+// };
+
+// const toStudyWithoutHost = async (pushEvent, study_id) => {
+//   const tokenRows = await pushDao.getMemberWithoutHostToken(study_id);
+//   send(tokenRows, pushEvent, study_id, study_title);
+// };
+
+// const toStudyWithoutUser = async (pushEvent, study_id, user_id) => {
+//   const tokenRows = await pushDao.getMemberWithoutUserToken(study_id, user_id);
+//   send(tokenRows, pushEvent, study_id);
+// };
+
+// //////////////
+
+const push = async (pushEvent, study_id, user_id) => {
+  const studyRows = Boolean(pushEvent === PushEventEnum.study_delete)
+    ? (studyRows = await pushDao.getStudyTitle(study_id))
+    : (studyRows = await studyDao.getStudy(study_id));
+  const study_title = studyRows[0].title;
+
+  const tokenRows = '';
+  const target = {
+    toHost: [PushEventEnum.apply_new],
+    toUser: [PushEventEnum.push_test, PushEventEnum.study_delete, PushEventEnum.apply_reject],
+    chat: [PushEventEnum.chat, PushEventEnum.apply_allow],
+    toStudyWithoutHost: [PushEventEnum.study_update, PushEventEnum.notice_new, PushEventEnum.notice_update],
+    toStudyWithoutUser: [PushEventEnum.study_delegate],
+  };
+
+  if (target.toHost.includes(pushEvent)) {
+    tokenRows = await pushDao.getHostToken(study_id);
+  } else if (target.toUser.includes(pushEvent)) {
+    tokenRows = await pushDao.getUserToken(user_id);
+  } else if (target.toStudyWithoutHost.includes(pushEvent)) {
+    tokenRows = await pushDao.getMemberWithoutHostToken(study_id);
+  } else if (target.toStudyWithoutUser.includes(pushEvent)) {
+    tokenRows = await pushDao.getMemberWithoutUserToken(study_id, user_id);
+  } else if (target.toStudyWithoutUser.includes(pushEvent)) {
+    tokenRows = await pushDao.getOffMemberToken(study_id, chat.nickname);
+  }
+  console.log('tokenRows, pushEvent, study_id, study_title: ', tokenRows, pushEvent, study_id, study_title);
+  send(tokenRows, pushEvent, study_id, study_title);
+};
+
+//////////
+
+const send = async (tokenRows, pushEvent, study_id, study_title) => {
   const { apnsPayload, fcmPayload } = getPushPayload(pushEvent, study_id);
   const insertData = {
     user_id: '',
     study_id,
+    study_title,
     pushEvent,
     message: apnsPayload.aps.alert,
   };
@@ -107,9 +139,10 @@ const fcmSender = (payload) => {
 
 module.exports = {
   chat,
-  toUser,
-  toHost,
-  toStudy,
-  toStudyWithoutHost,
-  toStudyWithoutUser,
+  push,
+  // toUser,
+  // toHost,
+  // toStudy,
+  // toStudyWithoutHost,
+  // toStudyWithoutUser,
 };
