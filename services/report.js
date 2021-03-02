b23df5e06d363fe70c93e84ea53c9fec5a84f00e
cheol-95 <rkdcjf0122@gmail.com>
2021-03-03 01:48:45 +0900

    feat: 스터디 신고 - 404에러 수정

--- services/report.js
+++ services/report.js
@@ -1,10 +1,16 @@
 const reportDao = require('../dao/report');
+const studyDao = require('../dao/study');
 const { customError } = require('../utils/errors/custom');
 
 const createReport = async ({ id: user_id }, { study_id }, { message }) => {
+  const studyRows = await studyDao.getStudy(study_id);
+  if (studyRows.length === 0) {
+    throw customError(404, '조회된 스터디가 없습니다');
+  }
+
   const reportRows = await reportDao.createReport({ study_id, user_id, message });
   if (reportRows.affectedRows === 0) {
-    throw customError(404, '조회된 스터디가 없습니다');
+    throw customError(400, '신고하기가 실패했습니다');
   }
 };
 
