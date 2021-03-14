367cc33074be5e8b56de7dcf663b64853639d15d
cheol-95 <rkdcjf0122@gmail.com>
2021-03-14 19:01:31 +0900

    fix: 스터디 지역 상세 공백 허용

--- middlewares/validators/study.js
+++ middlewares/validators/study.js
@@ -19,7 +19,7 @@ const createStudy = async (req, res, next) => {
     sigungu: Joi.string().max(20).required(),
     address_name: Joi.string().max(100).required(),
     place_name: Joi.string().allow('').max(30),
-    location_detail: Joi.string().max(30),
+    location_detail: Joi.string().allow('').max(30),
     study_time: Joi.string().required().max(100),
     image: Joi.allow(),
   });
@@ -64,7 +64,7 @@ const updateStudy = async (req, res, next) => {
     sigungu: Joi.string().max(20),
     address_name: Joi.string().max(100),
     place_name: Joi.string().allow('').max(30),
-    location_detail: Joi.string().max(30),
+    location_detail: Joi.string().allow('').max(30),
     study_time: Joi.string().max(100),
     image: Joi.allow(),
   }).min(1);
